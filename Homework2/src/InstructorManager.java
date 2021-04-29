244dfb0989694c9c6852d1b71ab9bb1998e3a411
humeyrakyy <humeyrakoyun3@gmail.com>
2021-04-29 18:19:23 +0300

    Third day

---
+++ Homework2/src/InstructorManager.java
@@ -0,0 +1,8 @@
+
+public class InstructorManager extends UserManager {
+	
+	@Override
+	public void add(User user) {
+		System.out.println("Instructor "+ user.getFirstName() +" " + user.getLastName() +" added.");
+	}
+}
---
+++ Homework2/src/UserManager.java
@@ -0,0 +1,13 @@
+
+public class UserManager {
+	
+	public void add(User user) {
+		System.out.println("Kullanýcý "+ user.getId() +" eklendi.");
+	}
+	
+	public void addMultiple(User[] users) {
+		for(User user : users) {
+			add(user);
+		}
+	}
+}
